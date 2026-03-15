70b06546c04ee812607206002645f39c6f15f752
WebbyDesigner <arnavpatkar728@gmail.com>
2026-03-15 21:03:28 +0530

    jdj

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-function filterProducts(category) {
-
-    let cards = document.querySelectorAll(".card")
-
-    cards.forEach(card => {
-
-        card.style.display = "none"
-
-        if (category === "all" || card.classList.contains(category)) {
-            card.style.display = "block"
-        }
-
-    })
-
-}
-
-function toggleMenu() {
+function toggleMenu(){
 
     let nav = document.getElementById("navMenu")
-
+    
     nav.classList.toggle("active")
-
-}
+    
+    }
+    
+    function filterProducts(category){
+    
+    let cards = document.querySelectorAll(".card")
+    
+    cards.forEach(card=>{
+    
+    card.style.display="none"
+    
+    if(category==="all" || card.classList.contains(category)){
+    card.style.display="block"
+    }
+    
+    })
+    
+    }
