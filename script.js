07e341cd29ead68e0ed283ea7ecb70604543681f
WebbyDesigner <arnavpatkar728@gmail.com>
2026-03-15 20:55:32 +0530

    third

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-function filterProducts(category){
+function filterProducts(category) {
 
     let cards = document.querySelectorAll(".card")
-    
-    cards.forEach(card=>{
-    
-    card.style.display="none"
-    
-    if(category==="all" || card.classList.contains(category)){
-    card.style.display="block"
-    }
-    
+
+    cards.forEach(card => {
+
+        card.style.display = "none"
+
+        if (category === "all" || card.classList.contains(category)) {
+            card.style.display = "block"
+        }
+
     })
-    
-    }
+
+}
+
+function toggleMenu() {
+
+    let nav = document.getElementById("navMenu")
+
+    nav.classList.toggle("active")
+
+}
